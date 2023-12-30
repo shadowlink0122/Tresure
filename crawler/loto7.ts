6b176a16d2131c410a5254e091496751359b8a8b
宮島健太 <shadowlink@miyajimakentanoMacBook-Pro.local>
2023-12-30 19:04:56 +0900

    ロト7のクローラ

--- crawler/loto7.ts
+++ crawler/loto7.ts
@@ -1,7 +1,6 @@
 import { getHtmlFromURL, writeToLocalFileSync } from './constants';
 import { LOTO7 } from '@/types/loto';
 import { LOTO7_FILENAME } from './constants';
-import iconv from 'iconv-lite';
 
 // URL
 /**
@@ -17,27 +16,32 @@ import iconv from 'iconv-lite';
  * ロト7
  * https://www.mizuhobank.co.jp/retail/takarakuji/check/loto/backnumber/detail.html?fromto=1_20&type=loto7
  */
-let START_IMPLEMENT = 1;
-const impNumber = String(START_IMPLEMENT.toString()).padStart(4, '0');
-const DATA_SOURCE_BASE_URL = `https://www.mizuhobank.co.jp/retail/takarakuji/loto/loto7/csv/A103${impNumber}.CSV`;
 
 // loto variables
 const loto7Data: LOTO7[] = [];
 
 async function crawl() {
+  let START_IMPLEMENT = 1;
   // データソースのHTMLを取得
   while (true) {
-    // 実施回数は4桁にする
+    // 実施回数は4桁に合わせる
+    const impNumber = String(START_IMPLEMENT.toString()).padStart(4, '0');
+    // URL
+    const DATA_SOURCE_BASE_URL = `https://www.mizuhobank.co.jp/retail/takarakuji/loto/loto7/csv/A103${impNumber}.CSV`;
+    // htmlを取得する
     const html = await getHtmlFromURL(DATA_SOURCE_BASE_URL);
     if (html === null) {
       break;
     }
-
-    // console.log(iconv.decode(Buffer.from(html), 'eucjp'));
-    console.log(html);
-
+    // 余計な改行をコンマに置き換えて、コンマで分ける
+    const splitedHtml = html.replace(/\r?\n/g, ',').split(',');
+    const loto7: LOTO7 = {
+      implemention: `第${START_IMPLEMENT}回`,
+      mainNumber: splitedHtml.slice(8, 15).map(Number),
+      bonusNumber: splitedHtml.slice(16, 18).map(Number),
+    }
+    loto7Data.push(loto7);
     START_IMPLEMENT += 1;
-    break;
   }
 }
 
