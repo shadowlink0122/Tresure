c21cb84d3a8590dc3554af9363e5d0217bd91195
shadowlink0122 <shadowlink_0122@yahoo.co.jp>
2023-12-31 22:56:14 +0900

    任意の数字と一緒に出る数字

---
+++ src/analyze/loto7/depOccurrence.ts
@@ -0,0 +1,71 @@
+import { getSavedLoto7DataSync } from "@/db/file";
+
+export type Pair = {
+  parent: number,
+  count: number,
+};
+
+export type DepOccurDict = {
+  [key: number]: Pair[]
+};
+
+const resultDict: DepOccurDict = {}
+
+function setNumber(key: number, value: number) {
+  if (resultDict[key] === undefined) {
+    resultDict[key] = [{ parent: value, count: 1 }];
+  } else {
+    const pair = resultDict[key].find(elm => elm.parent === value);
+    // 見つかった場合、カウントを増やす
+    if (pair !== undefined) {
+      pair.count += 1;
+      return;
+    }
+    // 見つからなかった場合、1を入れる
+    resultDict[key].push({
+      parent: value,
+      count: 1,
+    });
+  }
+}
+
+function createDepOccurDict() {
+  const fileContent = getSavedLoto7DataSync().slice(530);
+  // 数字を辞書に入れていく
+  console.log('set pair number.');
+  for (const loto7 of fileContent) {
+    for (let i = 0; i < 7; i += 1) {
+      for (let j = i + 1; j < 7; j += 1) {
+        setNumber(loto7.mainNumber[i], loto7.mainNumber[j]);
+        setNumber(loto7.mainNumber[j], loto7.mainNumber[i]);
+      }
+    }
+  }
+  console.log('sorting');
+  // キーごとに関連する数字をソートする
+  const dictKeys = Object.keys(resultDict);
+  for (const key of dictKeys) {
+    resultDict[Number(key)].sort((a: Pair, b: Pair) => {
+      if (a.count > b.count) return -1;
+      else if (a.count < b.count) return 1;
+      else if (a.parent < b.parent) return 1;
+      return 0;
+    });
+  }
+  console.log("dep:");
+  for (const dkey of dictKeys) {
+    const key = Number(dkey);
+    process.stdout.write(`${key}:`);
+    for (let i = 0; i < Math.min(resultDict[key].length, 7); i += 1) {
+      const pair = resultDict[key][i];
+      process.stdout.write(` ${pair.parent}(${pair.count})`);
+    }
+    console.log('');
+  }
+}
+
+function main() {
+  createDepOccurDict();
+}
+
+main();
