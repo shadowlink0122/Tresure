f9f25447664eafd55134cf2bcac1b43491d1459b
shadowlink0122 <shadowlink_0122@yahoo.co.jp>
2024-01-02 17:34:41 +0900

    export LOTO7Validator

--- src/types/loto7.ts
+++ src/types/loto7.ts
@@ -13,15 +13,11 @@ export type {
 // ロト7
 export const MAX_LOTO7_NUMBER = 37;
 export const Loto7NumberValidator = PositiveNumberValidator.min(1).max(MAX_LOTO7_NUMBER);
-const LOTO7Validator = z.object({
+export const LOTO7Validator = z.object({
   implemention: z.string().min(3),
   date: z.string(), // TODO: Date型(yyyy/mm/dd)に変換する
   mainNumber: z.array(Loto7NumberValidator).length(7),
   bonusNumber: z.array(Loto7NumberValidator).length(2),
 });
 
-type LOTO7 = z.infer<typeof LOTO7Validator>;
-
-export type {
-  LOTO7,
-}
+export type LOTO7 = z.infer<typeof LOTO7Validator>;
