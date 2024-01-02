8eecef5fcd940ddfaf056ee15154df94523aabf7
shadowlink0122 <shadowlink_0122@yahoo.co.jp>
2024-01-02 22:55:53 +0900

    汎用的な関数を作成

--- src/types/api/loto7/analyze/appearence.ts
+++ src/types/api/loto7/analyze/appearence.ts
@@ -3,15 +3,12 @@ import { Loto7NumberValidator } from '@/types/loto7';
 import { NextApiRequest } from 'next';
 import { TresureResponseBase } from '../../tresure_response_base';
 import { Loto7Validator } from '@/types/loto7';
+import { isUniqueArray } from '@/types/common';
 
 // リクエスト型
 export const SearchAppearenceRequestParamsValidator = z.object({
   is_main_number: z.boolean(),
-  numbers: Loto7NumberValidator.array().min(1).refine(item => {
-    // 要素はユニークである必要がある
-    const numSet = new Set(item);
-    return numSet.size === item.length;
-  })
+  numbers: Loto7NumberValidator.array().min(1).refine(isUniqueArray)
 });
 export type SearchAppearenceRequestParams = z.infer<typeof SearchAppearenceRequestParamsValidator>;
 export interface SearchAppearenceRequest extends NextApiRequest {
--- src/types/common.ts
+++ src/types/common.ts
@@ -2,3 +2,10 @@ import z from 'zod';
 
 // 共通
 export const PositiveIntNumberValidator = z.number().int().positive();
+
+// 配列がユニークかを確認する
+export function isUniqueArray<T>(item: T[]): boolean {
+  // 要素はユニークである必要がある
+  const numSet = new Set(item);
+  return numSet.size === item.length;
+}
