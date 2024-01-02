49e0dfacb23c6d05193e97b8a50a6e331bdb99f0
shadowlink0122 <shadowlink_0122@yahoo.co.jp>
2024-01-02 12:43:09 +0900

    specific continuous

---
+++ src/analyze/loto7/specific_continuous.ts
@@ -0,0 +1,27 @@
+import { MAX_LOTO7_NUMBER } from "../../types/loto7";
+import { continuous, checkContinueNumber } from "./continuous";
+import { getSavedLoto7DataSync } from "../../db/file";
+import fs from 'fs';
+import { BASE_DIR } from "../../../crawler/constants";
+import { join } from "path";
+import { Loto7ContinuousNumber } from "@/types/analyze/loto7/continuous";
+
+function getSpecificContinuous(loto7: Loto7ContinuousNumber[], checkNum: number) {
+  return loto7.filter(item => item.sameMainNumber.includes(checkNum) || item.sameBonusNumber.includes(checkNum));
+}
+
+function main() {
+  const fileContent = getSavedLoto7DataSync();
+  const continuousNumber = checkContinueNumber(continuous(fileContent), 1, false);
+  for (let i = 1; i < MAX_LOTO7_NUMBER; i += 1) {
+    const result = getSpecificContinuous(continuousNumber, i);
+    const dir = join(BASE_DIR, 'specific_continuous');
+    fs.mkdirSync(dir, { recursive: true });
+    fs.writeFileSync(
+      join(dir, `${i}.json`),
+      JSON.stringify(result, null, 2)
+    );
+  }
+}
+
+main();
