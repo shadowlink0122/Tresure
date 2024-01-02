9a360ff714f042ddc44fa5d00130bf69dc661b6c
shadowlink0122 <shadowlink_0122@yahoo.co.jp>
2024-01-02 23:56:59 +0900

    fix import

--- src/interface/api/search/loto7/appearence.ts
+++ src/interface/api/search/loto7/appearence.ts
@@ -1,7 +1,6 @@
-import z from 'zod';
 import { TresureResponseBase } from '@/types/api/tresure_response_base';
 import { NextApiRequest } from "next";
-import { SearchAppearenceRequestParams, SearchAppearenceResponseParams } from "@/types/api/loto7/search/appearence";
+import { SearchAppearenceRequestParams, SearchAppearenceResponseParams } from "@/types/api/search/loto7/appearence";
 
 export interface SearchAppearenceRequest extends NextApiRequest {
   body: SearchAppearenceRequestParams,
