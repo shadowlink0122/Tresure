37682c47cd7aaa01a7e90888403ef7f6f5818a8d
shadowlink0122 <shadowlink_0122@yahoo.co.jp>
2024-01-04 03:48:06 +0900

    ラベリングアルゴリズムを修正

--- src/libs/search/loto7/all_number_appearence.ts
+++ src/libs/search/loto7/all_number_appearence.ts
@@ -1,6 +1,85 @@
 import { NumberCountParams } from "@/types/api/search/loto7/all_number_appearence";
 import { LOTO7, MAX_LOTO7_NUMBER } from "@/types/loto7";
 
+function addVolume(result: NumberCountParams[], isMainNumber: boolean) {
+  // ラベル付けしないデータ個数の最大値を決める
+  let maxLeave = 0;
+  if (result.length === MAX_LOTO7_NUMBER) {
+    // 補充が存在しない場合、データは十分にあるので 
+    // 全体の最大 1/3 をラベル付けしない
+    maxLeave = MAX_LOTO7_NUMBER / 3
+  } else {
+    // 補充が存在する場合、データは十分にないので
+    // 出現した数の最大 1/2 をラベル付けしない
+    maxLeave = MAX_LOTO7_NUMBER / 2;
+  }
+
+
+  // 足りない分のデータ(count: 0)を補充
+  for (let i = 1; i <= MAX_LOTO7_NUMBER; i += 1) {
+    const found = result.filter(item => item.number === i);
+    if (found.length === 0) {
+      result.push({
+        number: i,
+        count: 0
+      });
+    }
+  }
+
+  // 最低限 'most' | 'less' をつける
+  let mostIndex = 0;
+  let lessIndex = 0;
+  let maxCount = 0;
+  let minCount = 0;
+  let totalChange = 0;
+  while (!((result.length - totalChange) <= maxLeave)) {
+    // mostを検索
+    // カウント順に降順ソート
+    result.sort((a, b) => (a.count < b.count) ? 1 : -1);
+    if (result[mostIndex].volume !== undefined) break;
+    maxCount = result[mostIndex].count;
+    while (mostIndex < result.length && result[mostIndex].count >= maxCount) {
+      result[mostIndex].volume = 'most';
+      mostIndex += 1;
+      totalChange += 1
+    }
+    // lessを検索
+    // カウント順に昇順ソート
+    result.sort((a, b) => (a.count > b.count) ? 1 : -1);
+    if (result[lessIndex].volume !== undefined) break;
+    minCount = result[lessIndex].count;
+    while (lessIndex < result.length && minCount >= result[lessIndex].count) {
+      result[lessIndex].volume = 'less';
+      lessIndex += 1;
+      totalChange += 1
+    }
+
+    // ここから下はラベル付けしなくて良い値
+    // 基準を超えていれば break
+    if ((result.length - totalChange) <= maxLeave) break;
+    // manyを検索
+    // カウント順に降順ソート
+    result.sort((a, b) => (a.count < b.count) ? 1 : -1);
+    if (result[mostIndex].volume !== undefined) break;
+    maxCount = result[mostIndex].count;
+    while (mostIndex < result.length && result[mostIndex].count >= maxCount) {
+      result[mostIndex].volume = 'many';
+      mostIndex += 1;
+      totalChange += 1
+    }
+    // fewを検索
+    // カウント順に昇順ソート
+    result.sort((a, b) => (a.count > b.count) ? 1 : -1);
+    if (result[lessIndex].volume !== undefined) break;
+    minCount = result[lessIndex].count;
+    while (lessIndex < result.length && minCount >= result[lessIndex].count) {
+      result[lessIndex].volume = 'few';
+      lessIndex += 1;
+      totalChange += 1
+    }
+  }
+}
+
 export function getAllNumberAppearence(loto7: LOTO7[], isMainNumber: boolean) {
   const result: NumberCountParams[] = [];
   // インデックス順に並び替える
@@ -30,64 +109,7 @@ export function getAllNumberAppearence(loto7: LOTO7[], isMainNumber: boolean) {
     });
   }
 
-  // 確認する回数(最大3)
-  const check_volume_times = Math.max(
-    1,
-    Math.trunc(result.length / 15)
-  );
-
-  // 足りない分のデータ(count: 0)を補充
-  for (let i = 1; i <= MAX_LOTO7_NUMBER; i += 1) {
-    const found = result.filter(item => item.number === i);
-    if (found.length === 0) {
-      result.push({
-        number: i,
-        count: 0
-      });
-    }
-  }
-
-  // カウント順に降順ソート
-  result.sort((a, b) => (a.count < b.count) ? 1 : -1);
-  // mostを検索
-  let maxCount = 0;
-  let nowIndex = 0;
-  for (let i = 0; i < check_volume_times; i += 1) {
-    maxCount = result[nowIndex].count;
-    while (result[nowIndex].count >= maxCount) {
-      result[nowIndex].volume = 'most';
-      nowIndex += 1;
-    }
-  }
-  // manyを検索
-  for (let i = 0; i < check_volume_times; i += 1) {
-    maxCount = result[nowIndex].count;
-    while (result[nowIndex].count >= maxCount) {
-      result[nowIndex].volume = 'many';
-      nowIndex += 1;
-    }
-  }
-
-  // カウント順に昇順ソート
-  result.sort((a, b) => (a.count > b.count) ? 1 : -1);
-  // lessを検索
-  let minCount = 0;
-  nowIndex = 0;
-  for (let i = 0; i < check_volume_times; i += 1) {
-    minCount = result[nowIndex].count;
-    while (minCount >= result[nowIndex].count) {
-      result[nowIndex].volume = 'less';
-      nowIndex += 1;
-    }
-  }
-  // fewを検索
-  for (let i = 0; i < check_volume_times; i += 1) {
-    minCount = result[nowIndex].count;
-    while (minCount >= result[nowIndex].count) {
-      result[nowIndex].volume = 'few';
-      nowIndex += 1;
-    }
-  }
+  addVolume(result, isMainNumber);
 
   // 番号順に昇順ソート
   result.sort((a, b) => (a.number > b.number) ? 1 : -1);
